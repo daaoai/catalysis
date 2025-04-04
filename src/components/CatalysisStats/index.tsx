5c246c2adf76e46a7a79d4c70fc12d216d582275
Antariksh Das <136224176+Anta18@users.noreply.github.com>
2025-04-04 22:47:12 +0530

    fix:stats

--- src/components/CatalysisStats/index.tsx
+++ src/components/CatalysisStats/index.tsx
@@ -16,7 +16,7 @@ const CatalystStats: React.FC = () => {
   return (
     <div className="px-6 md:px-20 bg-white">
       <div className="mb-12 max-w-sm">
-        <h2 className="max-w-[16rem] text-[32px] md:text-[40px] mb-4 mt-10 font-sans font-medium text-[#323131]">
+        <h2 className="max-w-[16rem] text-3xl md:text-[40px] mb-4 mt-10 font-sans font-medium text-[#323131]">
           {pageHeading.title}
         </h2>
         <p className="dark:text-gray text-[#383737] max-w-md text-sm font-normal font-generalsans">
@@ -26,7 +26,7 @@ const CatalystStats: React.FC = () => {
 
       {/* Wrapper div to right-align the grid */}
       <div className="flex justify-end">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-6 max-w-3xl lg:max-w-5xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-3xl lg:max-w-5xl">
           {/* First Card */}
           <div className="relative w-full flex flex-col justify-center">
             <img
@@ -34,14 +34,12 @@ const CatalystStats: React.FC = () => {
               alt={firstCard.title}
               className="w-full h-auto rounded-lg"
             />
-
             <div className="absolute top-0 md:top-20 lg:top-28 left-0 w-full h-full p-4 md:p-6 z-10 flex flex-col rounded-lg gap-y-2">
               <p className="text-xs dark:text-white-50 text-black-10 uppercase px-3 font-generalsans">
                 {firstCard.title}
               </p>
               <h3
                 className={`text-[64px] md:text-4xl lg:text-[96px] font-semibold font-sans sm:leading-[80%] sm:tracking-[-4%] md:leading-normal md:tracking-normal ${
-
                   theme === "dark"
                     ? firstCard.textColor.dark
                     : firstCard.textColor.light
@@ -61,14 +59,12 @@ const CatalystStats: React.FC = () => {
               alt={secondCard.title}
               className="w-full h-auto rounded-lg"
             />
-
             <div className="absolute top-0 left-0 w-full h-full p-4 md:p-6 z-10 flex flex-col rounded-lg gap-y-2">
               <p className="text-xs dark:text-white-50 text-black-10 uppercase px-3 font-generalsans">
                 {secondCard.title}
               </p>
               <h3
                 className={`text-[64px] md:text-4xl lg:text-[96px] font-semibold font-sans sm:leading-[80%] sm:tracking-[-4%] md:leading-normal md:tracking-normal ${
-
                   theme === "dark"
                     ? secondCard.textColor.dark
                     : secondCard.textColor.light
@@ -86,14 +82,12 @@ const CatalystStats: React.FC = () => {
               alt={thirdCard.title}
               className="w-full h-auto rounded-lg"
             />
-
             <div className="absolute top-0 left-0 w-full h-full p-4 md:p-6 z-10 flex flex-col rounded-lg gap-y-2">
               <p className="text-xs dark:text-white-50 text-black-10 uppercase px-3 font-generalsans">
                 {thirdCard.title}
               </p>
               <h3
                 className={`text-[64px] md:text-4xl lg:text-[96px] font-semibold font-sans sm:leading-[80%] sm:tracking-[-4%] md:leading-normal md:tracking-normal ${
-
                   theme === "dark"
                     ? thirdCard.textColor.dark
                     : thirdCard.textColor.light
